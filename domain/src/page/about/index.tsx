603f6728cb19bccc052e8010273d479e93a640d3
vscing <vscing@foxmail.com>
2024-11-04 00:28:32 +0800

    ✨ feat: label组件

--- domain/src/page/about/index.tsx
+++ domain/src/page/about/index.tsx
@@ -1,4 +1,6 @@
+import { Icons } from "@/assets/icons";
 import Header from "@/components/Header";
+import { Label } from "@/shadcn-ui/ui/label";
 
 function About() {
   return <div>
@@ -7,7 +9,13 @@ function About() {
     <div>
       <div>导入或创建书签库</div>
       <div>
-        
+      <Label
+        htmlFor="paypal"
+        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+      >
+        <Icons.paypal className="mb-3 h-6 w-6" />
+        Paypal
+      </Label>
       </div>
     </div>
     
