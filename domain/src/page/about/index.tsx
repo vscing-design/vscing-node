1bf378d4bb42e91c536469e448ee9b32665bd518
vscing <vscing@foxmail.com>
2024-11-05 00:48:29 +0800

    ✨ feat: 增加form

--- domain/src/page/about/index.tsx
+++ domain/src/page/about/index.tsx
@@ -1,8 +1,32 @@
 import { Icons } from "@/assets/icons";
 import Header from "@/components/Header";
+import { Button } from "@/shadcn-ui/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/shadcn-ui/ui/form";
+import { Input } from "@/shadcn-ui/ui/input";
 import { Label } from "@/shadcn-ui/ui/label";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 function About() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return <div>
     <Header/>
     
@@ -17,6 +41,30 @@ function About() {
         Paypal
       </Label>
       </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+      
     </div>
     
   </div>
