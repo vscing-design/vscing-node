18dba11aeb191062cf89e2d18116984eb9241b90
vscing <513335394@qq.com>
2024-12-12 20:22:51 +0800

    Update App.tsx

--- domain/src/App.tsx
+++ domain/src/App.tsx
@@ -3,8 +3,15 @@ import { ThemeProvider } from "@/shadcn-ui/components/theme-provider";
 import 'dayjs/locale/zh-cn';
 import { RouterProvider} from "react-router-dom";
 import router from '@/routers';
+import localforage from "localforage";
 
 function App() {
+
+  useEffect(() => {
+    localforage.config({
+      name: 'vscing'
+    });
+  }, []);
   
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
